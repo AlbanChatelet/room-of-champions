b8b58f9a517eb8c85b13ef1b808de1043f40d5a6
AlbanChatelet <chateletalban20@gmail.com>
2024-09-17 10:43:22 +0200

    commit initial

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router/auto'
+import LogInOut from './components/LogInOut.vue'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <h1 class="text-2xl bg-slate-400">Template principale</h1>
+      <LogInOut/>
+      <nav>
+        <ul>
+          <RouterLink to="/">Accueil</RouterLink>
+          <RouterLink to="/About">About</RouterLink>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
---
+++ src/pages/equipes/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <header>
+    
+    <div class="wrapper">
+      <h1 class="text-2xl bg-slate-400">Page des équipes</h1>
+      
+    </div>
+    </header>
+</template>
