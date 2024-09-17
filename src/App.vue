8f3e4d09b81e39f2872f7bbb6652f92d40be34a3
AlbanChatelet <chateletalban20@gmail.com>
2024-09-17 14:55:16 +0200

    ajout du style sur la liste des équipes

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,27 @@ import LogInOut from './components/LogInOut.vue'
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
-      <h1 class="text-2xl bg-slate-400">Template principale</h1>
-      <LogInOut/>
+  <header class="bg-gradient-to-r from-blue-500 to-red-500 p-4">
+    <div class="container mx-auto flex justify-between items-center">
+      <h1 class="text-3xl font-bold text-white">Template principale</h1>
+      <LogInOut />
       <nav>
-        <ul>
-          <RouterLink to="/">Accueil</RouterLink>
-          <RouterLink to="/About">About</RouterLink>
-          <RouterLink to="/equipes">Équipes</RouterLink>
+        <ul class="flex space-x-8">
+          <li>
+            <RouterLink to="/" class="text-white hover:text-gray-200 transition-colors">Accueil</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/About" class="text-white hover:text-gray-200 transition-colors">About</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/equipes" class="text-white hover:text-gray-200 transition-colors">Équipes</RouterLink>
+          </li>
         </ul>
       </nav>
     </div>
   </header>
-  <main>
+  
+  <main class="py-10">
     <RouterView />
   </main>
 </template>
-
