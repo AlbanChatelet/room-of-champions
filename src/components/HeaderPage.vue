7e6732f17b107729baa3dcf0764218dfa6ea008d
AlbanChatelet <chateletalban20@gmail.com>
2025-01-10 15:56:37 +0100

    header fini

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -2,7 +2,7 @@
 import { RouterLink } from 'vue-router/auto';
 import { ref } from 'vue'; // Importer ref
 import MenuBurger from "@/assets/icons/MenuBurger.vue"; // Import du composant de l'icône burger
-
+import ProfileIcon from "@/assets/icons/profileIcon.vue";
 const isMenuOpen = ref(false);
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
@@ -11,23 +11,38 @@ const toggleMenu = () => {
 
 
 </script>
+<style>
+.fond_header {
+  background-image: url('@/assets/fond_header.webp');
+  background-size: cover;
+}
+</style>
 
+
+<style>
+.fond_header {
+  background: linear-gradient(to right, #001E20, #130926);
+}
+</style>
 
 <template>
-  <header class="bg-[#000000] text-black flex items-center py-4 md:py-0 justify-between fixed top-0 left-0 w-full shadow-md z-50">
+  <header class="fond_header text-black flex items-center pt-4 pb-0 justify-between fixed top-0 left-0 w-full shadow-md z-50">
     <!-- Logo -->
-    <div class="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/8">
+    <div class="absolute  top-6 left-1/2 transform -translate-x-1/2 -translate-y-1/10">
       <RouterLink to="/" class="flex items-center">
         <img class="h-32 md:h-40" src="../assets/logo.webp" alt="Logo" />
       </RouterLink>
     </div>
 
     <!-- Menu Desktop -->
-    <nav class="hidden md:flex items-center space-x-8 py-14 w-full pl-48 pr-64">
-      <ul class="flex space-x-8 bg-[#00C9C6] w-full justify-between items-center h-[60px]">
+    <nav class="hidden md:flex items-center space-x-8 py-14 w-full pl-24 pr-[550px]">
+      <div class="mr-12">
+        <ProfileIcon class="w-20 h-20" />
+      </div>
+      <ul class="flex space-x-32 bg-[#00C9C6] w-full justify-between items-center h-[60px] rounded-[16px]">
         <li>
           <button 
-            class="text-black rounded-lg flex items-center justify-center font-source-code-pro font-bold">
+            class="text-black rounded-lg flex pl-24 items-center justify-center font-source-code-pro font-bold">
             JEUX
           </button>
         </li>
@@ -43,23 +58,31 @@ const toggleMenu = () => {
             PLANNING
           </button>
         </li>
-        <ul class="flex space-x-8 bg-[#00C9C6] w-full justify-between items-center h-[60px]"></ul>
         <li>
           <RouterLink
             to="/evenements"
             class="text-black rounded-lg flex pl-48 items-center justify-center font-source-code-pro font-bold">
-            ÉVÈNEMENTS
+            ÉVÉNEMENTS
           </RouterLink>
         </li>
         <li>
           <button 
-            class="text-black rounded-lg flex items-center justify-center font-source-code-pro font-bold">
+            class="text-black rounded-lg flex items-center pr-24 justify-center font-source-code-pro font-bold">
             MATÉRIEL
           </button>
         </li>
       </ul>
+      
+      <!-- Bouton Inscription -->
+      <button 
+        class="bg-[#00C9C6] h-[60px] text-black font-bold py-2 px-6 rounded-[16px] shadow-md hover:bg-[#7aecea] transition-all duration-300">
+        INSCRIPTION
+      </button>
+      <button 
+        class="bg-[#FFFFFF] h-[60px] text-[#00C9C6] font-bold py-2 px-6 rounded-[16px] shadow-md hover:bg-[#e4ffff] transition-all duration-300">
+        CONNEXION
+      </button>
     </nav>
-
     <!-- Menu Burger pour mobile -->
     <button @click="toggleMenu" class="md:hidden flex items-center mr-4">
       <template v-if="isMenuOpen">
@@ -104,25 +127,5 @@ const toggleMenu = () => {
   </header>
 </template>
 
-<style>
-/* Transition personnalisée */
-.slide-fade-enter-active, .slide-fade-leave-active {
-  transition: all 0.4s ease;
-}
-.slide-fade-enter-from {
-  transform: translateY(-20px);
-  opacity: 0;
-}
-.slide-fade-enter-to {
-  transform: translateY(0);
-  opacity: 1;
-}
-.slide-fade-leave-from {
-  transform: translateY(0);
-  opacity: 1;
-}
-.slide-fade-leave-to {
-  transform: translateY(-20px);
-  opacity: 0;
-}
-</style>
+
+
