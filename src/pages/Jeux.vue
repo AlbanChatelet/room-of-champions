f82fae6744cb79a0b4ccac54c010af1667f72849
Imadecrypte <nail.dezzaz45@gmail.com>
2025-02-21 12:34:57 +0100

    Modification pages jeux

--- src/pages/Jeux.vue
+++ src/pages/Jeux.vue
@@ -10,62 +10,55 @@
       </h1>
     </div>
     <div class="w-[502px] h-[3px] bg-[#00c9c6]"></div>
-    <div class="flex text-xl justify-center mt-6 text-white font-black font-poppins pt-24">
-      <div>LES JEUX LES PLUS DEMANDÉS</div>
+    
+    <div class="flex justify-center mt-6 pt-24">
+      <h2 class="text-xl text-white font-black font-poppins">
+        LES JEUX LES PLUS DEMANDÉS
+      </h2>
     </div>
 
     <!-- Carrousel -->
-    <!-- Slider -->
-<div data-hs-carousel='{
-  "loadingClasses": "opacity-0",
-  "dotsItemClasses": "hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer dark:border-neutral-600 dark:hs-carousel-active:bg-blue-500 dark:hs-carousel-active:border-blue-500",
-  "isCentered": true,
-  "slidesQty": {
-    "xs": 1,
-    "lg": 2
-  }
-}' class="relative">
-  <div class="hs-carousel w-full overflow-hidden bg-white rounded-lg">
-    <div class="relative min-h-72 -mx-1">
-      <div class="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
-        <!-- Première image -->
-        <div class="hs-carousel-slide px-1">
-          <div class="flex justify-center h-full">
-            <img src="../assets/rocketleague.webp" alt="Jeu 1" class="w-full h-auto rounded-lg">
+    <div 
+      data-hs-carousel='{
+        "loadingClasses": "opacity-0",
+        "dotsItemClasses": "hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer dark:border-neutral-600 dark:hs-carousel-active:bg-blue-500 dark:hs-carousel-active:border-blue-500",
+        "isCentered": true,
+        "slidesQty": { "xs": 1, "lg": 2 }
+      }'
+      class="relative mt-10"
+    >
+      <div class="hs-carousel w-full overflow-hidden bg-white rounded-lg">
+        <div class="relative min-h-72 flex transition-transform duration-700">
+          <!-- Première image -->
+          <div class="hs-carousel-slide flex justify-center px-2">
+            <img src="../assets/rocketleague.webp" alt="Rocket League" class="w-full h-auto rounded-lg">
           </div>
-        </div>
-        <!-- Deuxième image -->
-        <div class="hs-carousel-slide px-1">
-          <div class="flex justify-center h-full">
-            <img src="../assets/valorant.webp" alt="Jeu 2" class="w-full h-auto rounded-lg">
+          <!-- Deuxième image -->
+          <div class="hs-carousel-slide flex justify-center px-2">
+            <img src="../assets/valorant.webp" alt="Valorant" class="w-full h-auto rounded-lg">
           </div>
-        </div>
-        <!-- Troisième image -->
-        <div class="hs-carousel-slide px-1">
-          <div class="flex justify-center h-full">
-            <img src="../assets/supersmashbros.webp" alt="Jeu 3" class="w-full h-auto rounded-lg">
+          <!-- Troisième image -->
+          <div class="hs-carousel-slide flex justify-center px-2">
+            <img src="../assets/supersmashbros.webp" alt="Super Smash Bros" class="w-full h-auto rounded-lg">
           </div>
         </div>
       </div>
+
+      <!-- Boutons de navigation -->
+      <button type="button" class="hs-carousel-prev absolute inset-y-0 start-0 flex items-center w-12 h-full text-white hover:bg-gray-800/50 rounded-s-lg">
+        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <path d="m15 18-6-6 6-6"></path>
+        </svg>
+      </button>
+      <button type="button" class="hs-carousel-next absolute inset-y-0 end-0 flex items-center w-12 h-full text-white hover:bg-gray-800/50 rounded-e-lg">
+        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <path d="m9 18 6-6-6-6"></path>
+        </svg>
+      </button>
+
+      <!-- Pagination -->
+      <div class="hs-carousel-pagination flex justify-center absolute bottom-3 w-full space-x-2"></div>
     </div>
-  </div>
-
-  <!-- Boutons de navigation -->
-  <button type="button" class="hs-carousel-prev absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 rounded-s-lg">
-    <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-      <path d="m15 18-6-6 6-6"></path>
-    </svg>
-  </button>
-  <button type="button" class="hs-carousel-next absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 rounded-e-lg">
-    <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-      <path d="m9 18 6-6-6-6"></path>
-    </svg>
-  </button>
-
-  <!-- Pagination -->
-  <div class="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 space-x-2"></div>
-</div>
-<!-- End Slider -->
-
+    <!-- Fin Carrousel -->
   </main>
 </template>
