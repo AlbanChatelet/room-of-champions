0d8cc6c761544308a65aafca260a518bba379b0c
AlbanChatelet <chateletalban20@gmail.com>
2025-02-26 15:08:50 +0100

    Ajout de l'affichage de l'icône des équipes dans la liste des équipes

--- src/pages/equipes/index.vue
+++ src/pages/equipes/index.vue
@@ -14,28 +14,33 @@ onMounted(async () => {
     console.error('Error fetching teams:', error)
   }
 })
+
+// Fonction pour récupérer l'URL de l'icône
+const getIconUrl = (equipe: EquipesResponse) => {
+  return equipe.icone ? pb.getFileUrl(equipe, equipe.icone) : null
+}
 </script>
 
 <template>
-  
-
   <main class="py-10 bg-gray-100">
     <RouterLink to="/equipes/edit/:id?">Créer une équipe</RouterLink>
     <div class="container mx-auto">
       <div v-if="equipes.length > 0" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         <div v-for="equipe in equipes" :key="equipe.id" class="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 flex flex-col justify-between h-full">
+          <!-- Image de l'équipe -->
+          <img v-if="equipe.icone" :src="getIconUrl(equipe)" :alt="`Icône de ${equipe.nom}`" class="w-full h-40 object-cover rounded-md mb-4">
+          
           <!-- Contenu de l'équipe -->
           <div>
             <h2 class="text-2xl font-semibold text-green-700 mb-2">{{ equipe.nom }}</h2>
             <div v-html="equipe.description" class="mb-4"></div>
           </div>
 
-          <!-- Bouton pour rediriger vers la page de détails de l'équipe, fixé en bas à gauche -->
+          <!-- Bouton pour voir les détails -->
           <RouterLink :to="`/equipes/${equipe.id}`" class="inline-block stroke-blue-500 text-red-500 px-4 py-2 rounded-md shadow hover:bg-slate-200 transition-colors mt-auto">
             Voir les détails
           </RouterLink>
         </div>
-        
       </div>
       <div v-else class="text-center text-gray-500">
         <p>Aucune équipe trouvée.</p>
@@ -43,4 +48,3 @@ onMounted(async () => {
     </div>
   </main>
 </template>
-
