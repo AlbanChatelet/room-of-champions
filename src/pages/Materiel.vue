4e6e68c0d538b43ec1f46ea04ada0b909f2bfcf1
Imadecrypte <nail.dezzaz45@gmail.com>
2025-03-08 15:04:33 +0100

    Style Matériel

--- src/pages/Materiel.vue
+++ src/pages/Materiel.vue
@@ -2,7 +2,11 @@
 </script>
 
 <template>
-  <div>
-    <h1>Materiel</h1>
-  </div>
+    <main class="bg-[#000011] p-6 md:p-24">
+        <h1 class="font-poppins font-black text-2xl md:text-4xl text-white pl-4 md:pl-16 pb-6">
+            LE MATÉRIEL DES <span class="text-[#00c9c6]">CHAMPIONS</span>
+        </h1>
+    <div class="w-full md:w-[502px] h-[3px] bg-[#00c9c6] ml-4 md:ml-16"></div>
+    
+</main>
 </template>
