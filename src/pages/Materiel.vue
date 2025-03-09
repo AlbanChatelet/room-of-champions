f2b4ee7e1fba56ed5848fadf263583dcebe9ef76
Imadecrypte <nail.dezzaz45@gmail.com>
2025-03-09 17:16:50 +0100

    Partie casque + ecran fini

--- src/pages/Materiel.vue
+++ src/pages/Materiel.vue
@@ -34,5 +34,27 @@
             </h2>
         </div>
     </div>
+    <div class="flex flex-col md:flex-row pt-10 items-center">
+        <div class="md:w-1/2">
+            <h1 class="font-poppins font-black text-2xl md:text-4xl text-white pl-4 md:pl-16 pb-6">
+                <span class="text-9xl m-10">03.</span><span class="text-[#00c9c6] pl-8">LE CASQUE</span>
+            </h1>
+            <h2 class="font-poppins text-xl md:text-2xl text-white pl-4 md:pl-16 pb-6 w-full">
+Logitech G Pro X Gaming. Entrez dans une symphonie sonore avec le Logitech G Pro X, un casque qui redéfinit l’immersion. Pensé pour les compétiteurs et les passionnés, il ne se contente pas de restituer le son : il le magnifie. Avec son design premium et son confort inégalé, le G Pro X vous transporte au cœur de l’action, où chaque détail compte et où chaque décision peut faire la différence.            </h2>
+        </div>
+        <div class="hidden md:block w-[2px] bg-gradient-to-b from-white to-[#000011] mx-8 self-stretch"></div> <!--Bar-->
+        <img src="@/assets/casque.png" alt="casque" class="md:w-1/2 w-full h-auto rounded-3xl mt-6 md:mt-0">
+    </div>
+    <div class="flex flex-col md:flex-row pt-10 items-center">
+        <img src="@/assets/ecran.png" alt="ecran" class="md:w-1/2 w-full h-auto rounded-3xl mt-6 md:mt-0">
+        <div class="hidden md:block w-[2px] bg-gradient-to-b from-white to-[#000011] mx-8 self-stretch"></div> <!--Bar-->
+        <div class="md:w-1/2">
+            <h1 class="font-poppins font-black text-2xl md:text-4xl text-white pl-4 md:pl-16 pb-6">
+                <span class="text-9xl m-10">04.</span><span class="text-[#00c9c6] pl-8">L'ÉCRAN</span>
+            </h1>
+            <h2 class="font-poppins text-xl md:text-2xl text-white pl-4 md:pl-16 pb-6 w-full">
+Ecran odyssez G3 24 “ led 165 Hz. Plongez au cœur de l'action avec le Samsung Odyssey G3 24 pouces, l'écran pensé pour les gamers exigeants et les créatifs audacieux. Grâce à son design futuriste et ses performances de pointe, il ne se contente pas de suivre le rythme... il le dicte.            </h2>
+        </div>
+    </div>
 </main>
 </template>
