cdaf6b067ebf151a62574834ba2e85ffaf5e7793
Imadecrypte <nail.dezzaz45@gmail.com>
2025-03-09 17:11:06 +0100

    Partie clavier fini

--- src/pages/Materiel.vue
+++ src/pages/Materiel.vue
@@ -22,5 +22,17 @@
         <div class="hidden md:block w-[2px] bg-gradient-to-b from-white to-[#000011] mx-8 self-stretch"></div> <!--Bar-->
         <img src="@/assets/souris.png" alt="souris" class="md:w-1/2 w-full h-auto rounded-3xl mt-6 md:mt-0">
     </div>
+    <div class="flex flex-col md:flex-row pt-10 items-center">
+        <img src="@/assets/clavier.png" alt="clavier" class="md:w-1/2 w-full h-auto rounded-3xl mt-6 md:mt-0">
+        <div class="hidden md:block w-[2px] bg-gradient-to-b from-white to-[#000011] mx-8 self-stretch"></div> <!--Bar-->
+        <div class="md:w-1/2">
+            <h1 class="font-poppins font-black text-2xl md:text-4xl text-white pl-4 md:pl-16 pb-6">
+                <span class="text-9xl m-10">02.</span><span class="text-[#00c9c6] pl-8">LE CLAVIER</span>
+            </h1>
+            <h2 class="font-poppins text-xl md:text-2xl text-white pl-4 md:pl-16 pb-6 w-full">
+                Clavier Logitech G213. Découvrez le Logitech G213, le clavier qui allie performance et élégance. Conçu pour les joueurs et les créatifs, ses touches réactives et son rétroéclairage RGB personnalisable apportent un confort et une précision inégalés. Robuste et moderne, il transforme chaque pression en un pas vers la victoire.
+            </h2>
+        </div>
+    </div>
 </main>
 </template>
