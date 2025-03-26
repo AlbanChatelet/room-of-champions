076d473242c48e0c3dc5dd466b66c2aa6649ba97
AlbanChatelet <chateletalban20@gmail.com>
2025-03-26 11:19:59 +0100

    ajout du dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,17 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    VueRouter(),
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [VueRouter(), vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  // Décommenter pour build en mode production directement dans PocketBase
+  build: {
+    outDir: './dist' // Change le chemin ici pour que le build se fasse dans le dossier dist
   }
-
-
-  // // décommenter pour build en mode production directement dans pocketbase
- ,
-build: {
-outDir: './pocketbase/pb_public'
- }
-});
+})
